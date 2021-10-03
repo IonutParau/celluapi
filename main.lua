7412e47d54c1bde2b54dc74122db102098a10b91
Parau Ionut-Alexandru <ionut.alex.parau@gmail.com>
2021-10-03 13:05:25 +0300

    Fixed bugs with driller

--- main.lua
+++ main.lua
@@ -2850,19 +2850,28 @@ function DoDriller(x,y,dir)
 	local x2,y2 = x,y
 	if dir == 0 then x2 = x+1 elseif dir == 2 then x2 = x-1 end
 	if dir == 1 then y2 = y+1 elseif dir == 3 then y2 = y-1 end
-	if cells[y2][x2].ctype ~= 11 and cells[y2][x2].ctype ~= 50 and cells[y2][x2].ctype ~= -1 and cells[y2][x2].ctype ~= 40 then
+	if cells[y2][x2].ctype ~= 11 and cells[y2][x2].ctype ~= 50 and cells[y2][x2].ctype ~= -1 and cells[y2][x2].ctype ~= 40 and not isModdedTrash(cells[y2][x2].ctype) then
+		if cells[y2][x2].ctype > initialCellCount then
+			local canPush = canPushCell(x2, y2, x, y, "drill")
+			if not canPush then
+				return
+			end
+		end
 		local oldcell = CopyCell(x,y)
 		cells[y][x] = CopyCell(x2,y2)
 		cells[y2][x2] = oldcell
 		SetChunk(x,y,cells[y][x].ctype)
 		SetChunk(x2,y2,cells[y2][x2].ctype)
-	elseif cells[y2][x2].ctype == 11 or cells[y2][x2].ctype == 50 then
+	elseif cells[y2][x2].ctype == 11 or cells[y2][x2].ctype == 50 or isModdedTrash(cells[y2][x2].ctype) then
 		cells[y][x].ctype = 0
 		if cells[y2][x2].ctype == 50 then
 			if x2 < width-1 and (not cells[y2][x2+1].protected and cells[y2][x2+1].ctype ~= -1 and cells[y2][x2+1].ctype ~= 11 and cells[y2][x2+1].ctype ~= 40 and cells[y2][x2+1].ctype ~= 50) then cells[y2][x2+1].ctype = 0 end
 			if x2 > 0 and (not cells[y2][x2-1].protected and cells[y2][x2-1].ctype ~= -1 and cells[y2][x2-1].ctype ~= 11 and cells[y2][x2-1].ctype ~= 40 and cells[y2][x2-1].ctype ~= 50) then cells[y2][x2-1].ctype = 0 end
 			if y2 < height-1 and (not cells[y2+1][x2].protected and cells[y2+1][x2].ctype ~= -1 and cells[y2+1][x2].ctype ~= 11 and cells[y2+1][x2].ctype ~= 40 and cells[y2+1][x2].ctype ~= 50) then cells[y2+1][x2].ctype = 0 end
 			if y2 > 0 and (not cells[y2-1][x2].protected and cells[y2-1][x2].ctype ~= -1 and cells[y2-1][x2].ctype ~= 11 and cells[y2-1][x2].ctype ~= 40 and cells[y2-1][x2].ctype ~= 50) then cells[y2-1][x2].ctype = 0 end
+		end
+		if isModdedTrash(cells[y2][x2].ctype) then
+			modsOnTrashEat(cells[y2][x2].ctype, x2, y2, cells[y][x], x, y)
 		end
 		love.audio.play(destroysound)
 	end
